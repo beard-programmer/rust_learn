74c77597890e1909638dc2235d1016ed83e4eb21
victorshinkevich <supostatekx@gmail.com>
2022-11-27 15:24:29 +0300

    add nodes countable trait and refactor tests

--- binary_tree/src/lib.rs
+++ binary_tree/src/lib.rs
@@ -8,6 +8,35 @@ pub enum BinaryTree {
     Node(i32, Box<BinaryTree>, Box<BinaryTree>),
 }
 
+trait NodesCountable {
+    fn count_nodes(&self) -> u32;
+}
+impl NodesCountable for DumbTree {
+    fn count_nodes(&self) -> u32 {
+        fn count(tree: &DumbTree) -> u32 {
+            match tree {
+                DumbTree::Node(left, right) => 1 + count(&*left) + count(&*right),
+                DumbTree::Empty => 0,
+            }
+        }
+
+        count(self)
+    }
+}
+
+impl NodesCountable for BinaryTree {
+    fn count_nodes(&self) -> u32 {
+        fn count(tree: &BinaryTree) -> u32 {
+            match tree {
+                BinaryTree::Node(_, left, right) => 1 + count(&*left) + count(&*right),
+                BinaryTree::Empty => 0,
+            }
+        }
+
+        count(self)
+    }
+}
+
 pub fn get(seek_value: i32, tree: &BinaryTree) -> Option<&BinaryTree> {
     match tree {
         BinaryTree::Node(value, left, right) => match seek_value.cmp(&value) {
@@ -20,118 +49,141 @@ pub fn get(seek_value: i32, tree: &BinaryTree) -> Option<&BinaryTree> {
 }
 
 #[cfg(test)]
-mod get_tests {
+mod tests {
     use super::*;
 
-    fn build_tree_helper() -> crate::BinaryTree {
-        BinaryTree::Node(
-            50,
-            Box::new(BinaryTree::Node(
-                25,
-                Box::new(BinaryTree::Node(
-                    12,
-                    Box::new(BinaryTree::Empty),
-                    Box::new(BinaryTree::Empty),
-                )),
-                Box::new(BinaryTree::Node(
-                    37,
-                    Box::new(BinaryTree::Empty),
-                    Box::new(BinaryTree::Empty),
-                )),
-            )),
-            Box::new(BinaryTree::Node(
-                75,
-                Box::new(BinaryTree::Node(
-                    63,
-                    Box::new(BinaryTree::Empty),
-                    Box::new(BinaryTree::Empty),
-                )),
-                Box::new(BinaryTree::Node(
-                    100,
-                    Box::new(BinaryTree::Empty),
-                    Box::new(BinaryTree::Empty),
-                )),
-            )),
-        )
-    }
+    mod dumb_tree_tests {
+        use super::*;
 
-    #[test]
-    fn given_element_present() {
-        let seek = 37;
-        let tree = build_tree_helper();
-        let result = get(seek, &tree);
-        assert_eq!(result.is_some(), true);
-        if let BinaryTree::Node(result_value, _, _) = result.unwrap() {
-            assert_eq!(result_value.clone(), seek);
-        } else {
-            assert_eq!(1, -1); // Test is considered as failed if get function returns some result and its not equal to seek.
+        mod nodes_countable_tests {
+            use super::*;
+            #[test]
+            fn given_tree_of_seven() {
+                let tree_of_seven = helpers::build_dumb_tree();
+                let result = tree_of_seven.count_nodes();
+                assert_eq!(result, 7);
+            }
+
+            #[test]
+            fn given_empty_tree() {
+                let empty_tree = DumbTree::Empty;
+                let result = empty_tree.count_nodes();
+                assert_eq!(result, 0);
+            }
         }
     }
 
-    #[test]
-    fn given_element_is_absent() {
-        let seek = 100500;
-        let tree = build_tree_helper();
-        let result = get(seek, &tree);
-        assert_eq!(result.is_none(), true);
+    mod binary_tree_tests {
+        use super::*;
+
+        mod get_tests {
+            use super::*;
+            #[test]
+            fn given_element_present() {
+                let seek = 37;
+                let tree = helpers::build_binary_tree();
+                let result = get(seek, &tree);
+                assert_eq!(result.is_some(), true);
+                if let BinaryTree::Node(result_value, _, _) = result.unwrap() {
+                    assert_eq!(result_value.clone(), seek);
+                } else {
+                    assert_eq!(1, -1); // Test is considered as failed if get function returns some result and its not equal to seek.
+                }
+            }
+
+            #[test]
+            fn given_element_is_absent() {
+                let seek = 100500;
+                let tree = helpers::build_binary_tree();
+                let result = get(seek, &tree);
+                assert_eq!(result.is_none(), true);
+            }
+
+            #[test]
+            fn given_tree_is_empty() {
+                let seek = 37;
+                let tree = BinaryTree::Empty;
+                let result = get(seek, &tree);
+                assert_eq!(result.is_none(), true);
+            }
+        }
+
+        mod nodes_countable_tests {
+            use super::*;
+            #[test]
+            fn given_tree_of_seven() {
+                let tree_of_seven = helpers::build_binary_tree();
+                let result = tree_of_seven.count_nodes();
+                assert_eq!(result, 7);
+            }
+
+            #[test]
+            fn given_empty_tree() {
+                let empty_tree = BinaryTree::Empty;
+                let result = empty_tree.count_nodes();
+                assert_eq!(result, 0);
+            }
+        }
     }
 
-    #[test]
-    fn given_tree_is_empty() {
-        let seek = 37;
-        let tree = BinaryTree::Empty;
-        let result = get(seek, &tree);
-        assert_eq!(result.is_none(), true);
-    }
-}
+    mod helpers {
+        use super::*;
 
-pub fn count_nodes(tree: DumbTree) -> u32 {
-    match tree {
-        DumbTree::Node(left, right) => 1 + count_nodes(*left) + count_nodes(*right),
-        DumbTree::Empty => 0,
-    }
-}
-
-#[cfg(test)]
-mod count_nodes_tests {
-    use super::*;
-
-    fn build_tree_helper() -> crate::DumbTree {
-        DumbTree::Node(
-            Box::new(DumbTree::Node(
+        pub fn build_dumb_tree() -> crate::DumbTree {
+            DumbTree::Node(
                 Box::new(DumbTree::Node(
-                    Box::new(DumbTree::Empty),
-                    Box::new(DumbTree::Empty),
+                    Box::new(DumbTree::Node(
+                        Box::new(DumbTree::Empty),
+                        Box::new(DumbTree::Empty),
+                    )),
+                    Box::new(DumbTree::Node(
+                        Box::new(DumbTree::Empty),
+                        Box::new(DumbTree::Empty),
+                    )),
                 )),
                 Box::new(DumbTree::Node(
-                    Box::new(DumbTree::Empty),
-                    Box::new(DumbTree::Empty),
+                    Box::new(DumbTree::Node(
+                        Box::new(DumbTree::Empty),
+                        Box::new(DumbTree::Empty),
+                    )),
+                    Box::new(DumbTree::Node(
+                        Box::new(DumbTree::Empty),
+                        Box::new(DumbTree::Empty),
+                    )),
                 )),
-            )),
-            Box::new(DumbTree::Node(
-                Box::new(DumbTree::Node(
-                    Box::new(DumbTree::Empty),
-                    Box::new(DumbTree::Empty),
-                )),
-                Box::new(DumbTree::Node(
-                    Box::new(DumbTree::Empty),
-                    Box::new(DumbTree::Empty),
-                )),
-            )),
-        )
-    }
+            )
+        }
 
-    #[test]
-    fn given_tree_of_seven() {
-        let tree_of_seven = build_tree_helper();
-        let result = count_nodes(tree_of_seven);
-        assert_eq!(result, 7);
-    }
-
-    #[test]
-    fn given_empty_tree() {
-        let empty_tree = DumbTree::Empty;
-        let result = count_nodes(empty_tree);
-        assert_eq!(result, 0);
+        pub fn build_binary_tree() -> crate::BinaryTree {
+            BinaryTree::Node(
+                50,
+                Box::new(BinaryTree::Node(
+                    25,
+                    Box::new(BinaryTree::Node(
+                        12,
+                        Box::new(BinaryTree::Empty),
+                        Box::new(BinaryTree::Empty),
+                    )),
+                    Box::new(BinaryTree::Node(
+                        37,
+                        Box::new(BinaryTree::Empty),
+                        Box::new(BinaryTree::Empty),
+                    )),
+                )),
+                Box::new(BinaryTree::Node(
+                    75,
+                    Box::new(BinaryTree::Node(
+                        63,
+                        Box::new(BinaryTree::Empty),
+                        Box::new(BinaryTree::Empty),
+                    )),
+                    Box::new(BinaryTree::Node(
+                        100,
+                        Box::new(BinaryTree::Empty),
+                        Box::new(BinaryTree::Empty),
+                    )),
+                )),
+            )
+        }
     }
 }

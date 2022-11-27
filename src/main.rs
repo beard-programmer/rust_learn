bcb41a34564906cce57aba76c409039355f3f5bf
victorshinkevich <supostatekx@gmail.com>
2022-11-27 07:27:40 +0300

    use rectangle module in main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
 fn main() {
     println!("Hello, rust learner!\n");
     display_ownership::read_only();
+
+    let scale = 2;
+    let rectangle = rectangle::types::Rectangle {
+        width: dbg!(1 * scale) as rectangle::types::RectanglSide,
+        height: 2 as rectangle::types::RectanglSide,
+    };
+
+    dbg!(&rectangle);
+    dbg!(rectangle::calculate_area(rectangle));
+
+    let other_rectangle = dbg!(rectangle::types::Rectangle {
+        width: (1 * scale) as rectangle::types::RectanglSide,
+        height: 2 as rectangle::types::RectanglSide,
+    });
+
+    let area: rectangle::types::RectangleArea = rectangle::calculate_area(other_rectangle);
+    dbg!(area);
 }
